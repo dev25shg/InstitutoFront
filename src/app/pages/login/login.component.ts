b5a4ca789c19f5d6040ac6b5318bd68c7a83aeb9
Sergio Galvis <dev25shg@gmail.com>
2024-04-08 20:37:24 -0500

    cambios login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,38 @@
+import { HttpClient } from '@angular/comon/http';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/comon/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: Login;
+  constructor() {
+    this.loginObj= new Login();
+  }
+  onLogin() {
+    this.http.Get('https://localhost:7027/api/Estudiante/gh%2Cghhj?correo=gh&pass=ghhj', this.loginObj).suscribe((res:any)=>{
+      if(res.result) {
+        
+      }
+    })
+
+  }
+}
+
+export class Login {
+Correo: string;
+Password: string;
+constructor() {
+  this.Correo='';
+  this.Password='';
+}
 
 }
+
+
